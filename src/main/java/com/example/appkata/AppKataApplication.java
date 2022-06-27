a05621f82909d402993afffac06bc042334fe27d
ejoongseok <ejoongseok@gmail.com>
2022-06-27 13:56:06 +0900

    Initial commit

---
+++ src/main/java/com/example/appkata/AppKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.appkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appkata/AppKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
