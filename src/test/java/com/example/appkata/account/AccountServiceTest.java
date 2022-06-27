9ffcd17faccf43fd6df1ee9c8918957d3642a237
ejoongseok <ejoongseok@gmail.com>
2022-06-27 14:23:06 +0900

    AccountService 사용자 정보 등록 테스트 케이스 given 추가

--- src/test/java/com/example/appkata/account/AccountServiceTest.java
+++ src/test/java/com/example/appkata/account/AccountServiceTest.java
@@ -4,12 +4,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.example.appkata.account.application.CreateAccountRequest;
+
 class AccountServiceTest {
 
 	@Test
 	@DisplayName("사용자 정보 등록")
 	void join_account_test() {
 		// given
+		String username = "joongseok";
+		String email = "ejoongseok@gamil.com";
+		CreateAccountRequest request = new CreateAccountRequest(username, email);
 
 		// when
 		Account account = accountService.join(request);
