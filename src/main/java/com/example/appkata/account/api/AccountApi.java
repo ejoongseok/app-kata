1c4ac151d7ec25e7980b47a488ba752745ad9705
ejoongseok <ejoongseok@gmail.com>
2022-06-27 14:13:43 +0900

    createAccount 기능 추가

--- src/main/java/com/example/appkata/account/api/AccountApi.java
+++ src/main/java/com/example/appkata/account/api/AccountApi.java
@@ -1,9 +1,22 @@
 package com.example.appkata.account.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.appkata.account.application.CreateAccountRequest;
+import com.example.appkata.account.application.CreateAccountResponse;
 
 @RestController
 @RequestMapping("/accounts")
 public class AccountApi {
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public CreateAccountResponse createAccount(@RequestBody CreateAccountRequest request) {
+		return null;
+	}
 }
