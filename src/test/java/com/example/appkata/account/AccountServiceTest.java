ebd2f26981fe6f41905fb0cd93659a31b24daf26
ejoongseok <ejoongseok@gmail.com>
2022-06-27 14:17:22 +0900

    AccountService 사용자 정보 등록 테스트 케이스 추가

--- src/test/java/com/example/appkata/account/AccountServiceTest.java
+++ src/test/java/com/example/appkata/account/AccountServiceTest.java
@@ -1,4 +1,19 @@
 package com.example.appkata.account;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class AccountServiceTest {
+
+	@Test
+	@DisplayName("사용자 정보 등록")
+	void join_account_test() {
+		// given
+
+		// when
+
+		// then
+	}
+
 }
