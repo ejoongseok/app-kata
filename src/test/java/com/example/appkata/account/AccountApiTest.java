58e26e495c0bf88a311b6cf81d33718b8201f63d
ejoongseok <ejoongseok@gmail.com>
2022-06-27 14:50:27 +0900

    유효하지 않은 이메일으로 사용자 등록을 요청하면 bad Request 응답 케이스 작성

--- src/test/java/com/example/appkata/account/AccountApiTest.java
+++ src/test/java/com/example/appkata/account/AccountApiTest.java
@@ -2,21 +2,30 @@ package com.example.appkata.account;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-import java.io.UnsupportedEncodingException;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.appkata.account.application.CreateAccountRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 class AccountApiTest {
+
+	MockMvc mockMvc;
+	ObjectMapper objectMapper;
 
 	@Test
 	@DisplayName("유효하지 않은 이메일 주소를 입력하면 Bad Request 응답.")
 	void should_invalid_email_join_account_return_throw() throws Exception {
 		// given
+		String username = "joongseok";
+		String email = "";
+		String exceptionMessage = "유효하지 않은 이메일 주소입니다.";
+		CreateAccountRequest request =  new CreateAccountRequest(username, email);
 
 
 		// when
@@ -29,7 +38,14 @@ class AccountApiTest {
 		Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
 		AccountExceptionResponse exceptionResponse = objectMapper.readValue(response.getContentAsString(),
 			AccountExceptionResponse.class);
-		Assertions.assertThat(exceptionResponse.getMessage()).isEqaulTo(exceptionMessage);
+		Assertions.assertThat(exceptionResponse.getMessage()).isEqualTo(exceptionMessage);
 	}
 
+	private static class AccountExceptionResponse {
+		private String message;
+
+		public String getMessage() {
+			return message;
+		}
+	}
 }
