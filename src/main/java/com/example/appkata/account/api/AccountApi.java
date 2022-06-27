e13e46f9f30328572bbcf5f44796a6010f1cae15
ejoongseok <ejoongseok@gmail.com>
2022-06-27 14:11:54 +0900

    AccountApi RestController 추가

---
+++ src/main/java/com/example/appkata/account/api/AccountApi.java
@@ -0,0 +1,9 @@
+package com.example.appkata.account.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountApi {
+}
