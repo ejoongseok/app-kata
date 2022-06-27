61469637cd2f25b7e124e87cb1ac537beeb34cbf
ejoongseok <ejoongseok@gmail.com>
2022-06-27 16:28:52 +0900

    로그인 테스트 케이스

--- src/test/java/com/example/appkata/login/LoginServiceTest.java
+++ src/test/java/com/example/appkata/login/LoginServiceTest.java
@@ -3,17 +3,27 @@ package com.example.appkata.login;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.example.appkata.login.application.LoginRequest;
+import com.example.appkata.login.application.LoginService;
+import com.example.appkata.login.application.LoginSession;
+
 class LoginServiceTest {
+
+	private LoginService loginService;
 
 	@Test
 	void 로그인() {
 		// given
+		String email = "ejoongseok@gmail.com";
+		String expectedUsername = "ejoongseok";
+		LoginRequest request = new LoginRequest(email);
 
 		// when
+		LoginSession loginSession = loginService.login(request);
 
 		// then
 		Assertions.assertThat(loginSession.getEmail()).isEqualTo(email);
-		Assertions.assertThat(loginSession.getUsername()).isEqualTo(username);
+		Assertions.assertThat(loginSession.getUsername()).isEqualTo(expectedUsername);
 	}
 
 }
