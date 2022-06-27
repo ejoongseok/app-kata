1d7cd12c7aaa5e6e5ed608ed0e38d9868173ef4a
ejoongseok <ejoongseok@gmail.com>
2022-06-27 17:18:49 +0900

    사용자 이름 수정 테스트 케이스 추가

--- src/test/java/com/example/appkata/account/AccountServiceTest.java
+++ src/test/java/com/example/appkata/account/AccountServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.appkata.account.application.AccountService;
 import com.example.appkata.account.application.CreateAccountRequest;
+import com.example.appkata.account.application.UpdateAccountRequest;
 import com.example.appkata.account.domain.Account;
 
 @SpringBootTest
@@ -31,5 +32,20 @@ class AccountServiceTest {
 		Assertions.assertThat(account.getUsername()).isEqualTo(username);
 		Assertions.assertThat(account.getEmail()).isEqualTo(email);
 	}
+
+	@Test
+	@DisplayName("사용자 이름 수정")
+	void update_username_test() {
+		// given
+		accountService.join(new CreateAccountRequest("joongseok", "joongseok@gamil.com");
+		String updateUsername = "joongSeok";
+		UpdateAccountRequest request = new UpdateAccountRequest(updateUsername);
+		// when
+		Account updateAccount = accountService.updateUsername(request);
+
+		// then
+		Assertions.assertThat(updateAccount.getUsername()).isEqualTo(updateUsername);
+	}
+
 
 }
