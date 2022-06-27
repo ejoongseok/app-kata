a9007a8c1e83d51b8804116978dcafdfd90d7d57
ejoongseok <ejoongseok@gmail.com>
2022-06-27 14:43:56 +0900

    유효하지 않은 이메일으로 사용자 등록을 요청하면 예외가 발생한다.

--- src/test/java/com/example/appkata/account/AccountApiTest.java
+++ src/test/java/com/example/appkata/account/AccountApiTest.java
@@ -1,4 +1,18 @@
 package com.example.appkata.account;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class AccountApiTest {
+
+	@Test
+	@DisplayName("유효하지 않은 이메일 주소를 입력하면 예외가 발생한다.")
+	void should_invalid_email_join_account_renturn_throw() {
+		// given
+
+		// when
+
+		// then
+	}
+
 }
