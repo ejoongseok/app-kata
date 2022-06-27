654aab4e35437f070693e9acf493e3997742d911
ejoongseok <ejoongseok@gmail.com>
2022-06-27 16:25:38 +0900

    로그인 테스트 케이스 Assertions 추가

--- src/test/java/com/example/appkata/login/LoginServiceTest.java
+++ src/test/java/com/example/appkata/login/LoginServiceTest.java
@@ -1,4 +1,19 @@
 package com.example.appkata.login;
 
-public class LoginServiceTest {
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LoginServiceTest {
+
+	@Test
+	void 로그인() {
+		// given
+
+		// when
+
+		// then
+		Assertions.assertThat(loginSession.getEmail()).isEqualTo(email);
+		Assertions.assertThat(loginSession.getUsername()).isEqualTo(username);
+	}
+
 }
