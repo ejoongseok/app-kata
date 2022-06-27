2cdc654fd9db016e463a9a9df1db5b77b22e11be
ejoongseok <ejoongseok@gmail.com>
2022-06-27 14:47:27 +0900

    유효하지 않은 이메일으로 사용자 등록을 요청하면 bad Request 응답 Assertions 작성

--- src/test/java/com/example/appkata/account/AccountApiTest.java
+++ src/test/java/com/example/appkata/account/AccountApiTest.java
@@ -1,18 +1,23 @@
 package com.example.appkata.account;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 class AccountApiTest {
 
 	@Test
-	@DisplayName("유효하지 않은 이메일 주소를 입력하면 예외가 발생한다.")
-	void should_invalid_email_join_account_renturn_throw() {
+	@DisplayName("유효하지 않은 이메일 주소를 입력하면 Bad Request 응답.")
+	void should_invalid_email_join_account_return_throw() {
 		// given
+
 
 		// when
 
 		// then
+		Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+		Assertions.assertThat(exceptionResponse.getMessage()).isEqaulTo(exceptionMessage);
 	}
 
 }
