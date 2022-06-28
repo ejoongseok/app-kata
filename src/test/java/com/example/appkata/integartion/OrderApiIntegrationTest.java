bb847c9b9408512064f32a12f34283c99454796a
ejoongseok <ejoongseok@gmail.com>
2022-06-28 16:30:30 +0900

    주문 요청 테스트 케이스 Assertions 작성

--- src/test/java/com/example/appkata/integartion/OrderApiIntegrationTest.java
+++ src/test/java/com/example/appkata/integartion/OrderApiIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.example.appkata.integartion;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 class OrderApiIntegrationTest {
 
@@ -11,6 +13,12 @@ class OrderApiIntegrationTest {
 		// when
 
 		// then
+		Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+		Assertions.assertThat(createOrderResponse.getOrderId()).isPositive();
+		Assertions.assertThat(createOrderResponse.getQuantity()).isEqualTo(quantity);
+		Assertions.assertThat(createOrderResponse.getTotalPrice()).isEqualTo(expectedTotalPrice);
+		Assertions.assertThat(createOrderResponse.getProductId()).isEqualTo(product.getId());
+		Assertions.assertThat(createOrderResponse.getProductName()).isEqualTo(product.getName());
 	}
 
 }
