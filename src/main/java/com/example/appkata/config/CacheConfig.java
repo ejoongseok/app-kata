f047996de18cfd69fb94f532be15413ca78eca06
ejoongseok <ejoongseok@gmail.com>
2022-06-28 10:55:49 +0900

    캐시 설정

---
+++ src/main/java/com/example/appkata/config/CacheConfig.java
@@ -0,0 +1,57 @@
+package com.example.appkata.config;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+import lombok.Getter;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+	@Bean
+	public CacheManager cacheManager() {
+		List<CaffeineCache> caches = Arrays.stream(AppCacheType.values())
+			.map(cache -> new CaffeineCache(cache.getCacheName(), Caffeine.newBuilder().recordStats()
+					.expireAfterWrite(cache.getExpireAfterWrite(), TimeUnit.SECONDS)
+					.maximumSize(cache.getMaximumSize())
+					.build()
+				)
+			)
+			.toList();
+
+		SimpleCacheManager cacheManager = new SimpleCacheManager();
+		cacheManager.setCaches(caches);
+
+		return cacheManager;
+	}
+
+	@Getter
+	public enum AppCacheType {
+		FIND_USER(
+				"findUser",
+				60 * 60 * 24, // 하루
+				100
+			);
+
+		AppCacheType(String cacheName, int expireAfterWrite, int maximumSize) {
+			this.cacheName = cacheName;
+			this.expireAfterWrite = expireAfterWrite;
+			this.maximumSize = maximumSize;
+		}
+		private final String cacheName;
+		private final int expireAfterWrite;
+		private final int maximumSize;
+	}
+}
