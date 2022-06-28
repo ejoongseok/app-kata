c8a97fe141dfff3613cd2a98a5b153985afd3af8
ejoongseok <ejoongseok@gmail.com>
2022-06-28 15:16:15 +0900

    added updateProduct api

--- src/main/java/com/example/appkata/module/product/api/ProductApi.java
+++ src/main/java/com/example/appkata/module/product/api/ProductApi.java
@@ -2,6 +2,7 @@ package com.example.appkata.module.product.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.appkata.module.product.application.CreateProductRequest;
 import com.example.appkata.module.product.application.CreateProductResponse;
 import com.example.appkata.module.product.application.ProductService;
+import com.example.appkata.module.product.application.UpdateProductRequest;
+import com.example.appkata.module.product.application.UpdateProductResponse;
 import com.example.appkata.module.product.domain.Product;
 
 import lombok.RequiredArgsConstructor;
@@ -26,5 +29,11 @@ public class ProductApi {
 	public CreateProductResponse create(@RequestBody CreateProductRequest request) {
 		Product product = productService.createProduct(request);
 		return new CreateProductResponse(product);
+	}
+
+	@PutMapping
+	@ResponseStatus(HttpStatus.OK)
+	public UpdateProductResponse updateProduct(@RequestBody UpdateProductRequest request) {
+		return null;
 	}
 }
