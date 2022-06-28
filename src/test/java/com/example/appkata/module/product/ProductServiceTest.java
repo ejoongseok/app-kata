f591cbe0f335546c084991d905d80f3cbdc81989
ejoongseok <ejoongseok@gmail.com>
2022-06-28 14:40:48 +0900

    상품 등록 서비스 테스트 케이스 Assertions 추가

--- src/test/java/com/example/appkata/module/product/ProductServiceTest.java
+++ src/test/java/com/example/appkata/module/product/ProductServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.appkata.module.product;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,10 @@ class ProductServiceTest {
 		// when
 
 		// then
+		Assertions.assertThat(product.getId()).isPositive();
+		Assertions.assertThat(product.getName()).isEqualTo(productName);
+		Assertions.assertThat(product.getPrice()).isEqualTo(price);
+
 	}
 
 }
