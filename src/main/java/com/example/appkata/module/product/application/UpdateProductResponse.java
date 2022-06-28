a10edae81aebaed6ee432f50ac973deb22e4bd11
ejoongseok <ejoongseok@gmail.com>
2022-06-28 15:32:25 +0900

    added constructor

--- src/main/java/com/example/appkata/module/product/application/UpdateProductResponse.java
+++ src/main/java/com/example/appkata/module/product/application/UpdateProductResponse.java
@@ -1,5 +1,7 @@
 package com.example.appkata.module.product.application;
 
+import com.example.appkata.module.product.domain.Product;
+
 import lombok.Getter;
 
 @Getter
@@ -7,4 +9,10 @@ public class UpdateProductResponse {
 	private Long id;
 	private String productName;
 	private int price;
+
+	public UpdateProductResponse(Product updateProduct) {
+		this.id = updateProduct.getId();
+		this.productName = updateProduct.getName();
+		this.price = updateProduct.getPrice();
+	}
 }
