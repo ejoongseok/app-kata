5149342f1a15c9ead23a4cc79648ae3200274d6c
ejoongseok <ejoongseok@gmail.com>
2022-06-28 14:43:17 +0900

    상품 등록 서비스 테스트 케이스 추가

--- src/test/java/com/example/appkata/module/product/ProductServiceTest.java
+++ src/test/java/com/example/appkata/module/product/ProductServiceTest.java
@@ -4,15 +4,21 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.example.appkata.module.product.application.CreateProductRequest;
+
 class ProductServiceTest {
+
+	private ProductService productService;
 
 	@Test
 	@DisplayName("상품 등록")
 	void create_product_test() {
 		// given
-
+		String productName = "노트북";
+		int price = 1_000_000;
+		CreateProductRequest request = CreateProductRequest.of(productName, price);
 		// when
-
+		Product product = productService.createProduct(request);
 		// then
 		Assertions.assertThat(product.getId()).isPositive();
 		Assertions.assertThat(product.getName()).isEqualTo(productName);
@@ -20,4 +26,27 @@ class ProductServiceTest {
 
 	}
 
+	private static class Product {
+		private Long id;
+		private String name;
+		private int price;
+
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public int getPrice() {
+			return price;
+		}
+	}
+
+	private class ProductService {
+		public Product createProduct(CreateProductRequest request) {
+			return null;
+		}
+	}
 }
