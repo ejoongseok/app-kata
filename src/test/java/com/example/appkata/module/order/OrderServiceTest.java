793919f3e28666b64f655ec97e7747c8fbae78d3
ejoongseok <ejoongseok@gmail.com>
2022-06-28 17:04:26 +0900

    상품 주문 테스트 케이스 작성

--- src/test/java/com/example/appkata/module/order/OrderServiceTest.java
+++ src/test/java/com/example/appkata/module/order/OrderServiceTest.java
@@ -1,23 +1,69 @@
 package com.example.appkata.module.order;
 
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.example.appkata.fixture.ProductFixture;
+import com.example.appkata.module.order.application.CreateOrderRequest;
+import com.example.appkata.module.product.domain.Product;
+
 class OrderServiceTest {
+
+	ProductFixture productFixture;
+	OrderService orderService;
 
 	@Test
 	void 상품_주문() {
 		// given
+		Product product = productFixture.createProduct("상품1", 100);
+
+		long productId = product.getId();
+		int quantity = 10;
+		int totalPrice = product.getPrice() * quantity;
+		CreateOrderRequest request = CreateOrderRequest.of(productId, quantity);
 
 		// when
+		Order order = orderService.order(request);
 
 		// then
 		Assertions.assertThat(order.getId()).isPositive();
 		Assertions.assertThat(order.getProductId()).isEqualTo(productId);
-		Assertions.assertThat(order.getProductName()).isEqualTo(productName);
+		Assertions.assertThat(order.getProductName()).isEqualTo(product.getName());
 		Assertions.assertThat(order.getQuantity()).isEqualTo(quantity);
 		Assertions.assertThat(order.getTotalPrice()).isEqualTo(totalPrice);
 
 	}
 
+	private static class OrderService {
+		public Order order(CreateOrderRequest request) {
+			return null;
+		}
+	}
+
+	private static class Order {
+		private Long id;
+		private Product product;
+		private int quantity;
+
+		public long getId() {
+			return id;
+		}
+
+		public String getProductName() {
+			return product.getName();
+		}
+
+		public int getQuantity() {
+			return quantity;
+		}
+
+		public int getTotalPrice() {
+			return product.getPrice() * quantity;
+		}
+
+		public Long getProductId() {
+			return product.getId();
+		}
+	}
 }
