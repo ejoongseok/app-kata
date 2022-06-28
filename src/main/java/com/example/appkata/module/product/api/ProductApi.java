d0094d59f0c31d995a13572dc81a7efc251ec9f0
ejoongseok <ejoongseok@gmail.com>
2022-06-28 15:54:58 +0900

    added getProduct method

--- src/main/java/com/example/appkata/module/product/api/ProductApi.java
+++ src/main/java/com/example/appkata/module/product/api/ProductApi.java
@@ -1,6 +1,8 @@
 package com.example.appkata.module.product.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.appkata.module.product.application.CreateProductRequest;
 import com.example.appkata.module.product.application.CreateProductResponse;
+import com.example.appkata.module.product.application.FindProductResponse;
 import com.example.appkata.module.product.application.ProductService;
 import com.example.appkata.module.product.application.UpdateProductRequest;
 import com.example.appkata.module.product.application.UpdateProductResponse;
@@ -36,5 +39,12 @@ public class ProductApi {
 	public UpdateProductResponse updateProduct(@RequestBody UpdateProductRequest request) {
 		Product updateProduct = productService.updateProduct(request);
 		return new UpdateProductResponse(updateProduct);
+	}
+
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public FindProductResponse getProduct(@PathVariable("id") Long id) {
+		Product product = productService.findProduct(id);
+		return new FindProductResponse(product);
 	}
 }
--- src/main/java/com/example/appkata/module/product/application/FindProductResponse.java
+++ src/main/java/com/example/appkata/module/product/application/FindProductResponse.java
@@ -1,10 +1,20 @@
 package com.example.appkata.module.product.application;
 
+import com.example.appkata.module.product.domain.Product;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class FindProductResponse {
 	private long id;
 	private String productName;
 	private int price;
+
+	public FindProductResponse(Product product) {
+		this.id = product.getId();
+		this.productName = product.getName();
+		this.price = product.getPrice();
+	}
 }
