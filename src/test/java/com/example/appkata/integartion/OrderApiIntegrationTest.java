5c1fc82582164d6eaef0fb090600710a3581588e
ejoongseok <ejoongseok@gmail.com>
2022-06-28 16:24:35 +0900

    주문 요청 테스트 케이스 작성

--- src/test/java/com/example/appkata/integartion/OrderApiIntegrationTest.java
+++ src/test/java/com/example/appkata/integartion/OrderApiIntegrationTest.java
@@ -1,4 +1,16 @@
 package com.example.appkata.integartion;
 
+import org.junit.jupiter.api.Test;
+
 class OrderApiIntegrationTest {
+
+	@Test
+	void 주문_요청() {
+		// given
+
+		// when
+
+		// then
+	}
+
 }
