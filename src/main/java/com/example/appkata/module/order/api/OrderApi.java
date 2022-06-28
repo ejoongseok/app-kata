7a6d644064e12897820c3e48de1626074ee5738b
ejoongseok <ejoongseok@gmail.com>
2022-06-28 17:21:25 +0900

    주문 api orderService 의존성 추가

--- src/main/java/com/example/appkata/module/order/api/OrderApi.java
+++ src/main/java/com/example/appkata/module/order/api/OrderApi.java
@@ -9,14 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.appkata.module.order.application.CreateOrderRequest;
 import com.example.appkata.module.order.application.CreateOrderResponse;
+import com.example.appkata.module.order.application.OrderService;
+import com.example.appkata.module.order.domain.Order;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderApi {
+
+	private final OrderService orderService;
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public CreateOrderResponse order(@RequestBody CreateOrderRequest request) {
-		return null;
+		Order order = orderService.order(request);
+		return new CreateOrderResponse(order);
 	}
 }
--- src/main/java/com/example/appkata/module/order/application/CreateOrderResponse.java
+++ src/main/java/com/example/appkata/module/order/application/CreateOrderResponse.java
@@ -1,8 +1,12 @@
 package com.example.appkata.module.order.application;
 
+import com.example.appkata.module.order.domain.Order;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class CreateOrderResponse {
 	private long orderId;
 	private long productId;
@@ -10,4 +14,11 @@ public class CreateOrderResponse {
 	private int totalPrice;
 	private String productName;
 
+	public CreateOrderResponse(Order order) {
+		this.orderId = order.getId();
+		this.productId = order.getProduct().getId();
+		this.quantity = order.getQuantity();
+		this.totalPrice = order.getTotalPrice();
+		this.productName = order.getProduct().getName();
+	}
 }
