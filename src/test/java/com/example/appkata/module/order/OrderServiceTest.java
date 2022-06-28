d70a2f66857934430f7a1332284361f80407a4f5
ejoongseok <ejoongseok@gmail.com>
2022-06-28 16:59:12 +0900

    상품 주문 테스트 케이스 Assertions 작성ㅂ

--- src/test/java/com/example/appkata/module/order/OrderServiceTest.java
+++ src/test/java/com/example/appkata/module/order/OrderServiceTest.java
@@ -1,4 +1,23 @@
 package com.example.appkata.module.order;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class OrderServiceTest {
+
+	@Test
+	void 상품_주문() {
+		// given
+
+		// when
+
+		// then
+		Assertions.assertThat(order.getId()).isPositive();
+		Assertions.assertThat(order.getProductId()).isEqualTo(productId);
+		Assertions.assertThat(order.getProductName()).isEqualTo(productName);
+		Assertions.assertThat(order.getQuantity()).isEqualTo(quantity);
+		Assertions.assertThat(order.getTotalPrice()).isEqualTo(totalPrice);
+
+	}
+
 }
