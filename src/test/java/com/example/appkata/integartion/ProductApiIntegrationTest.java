d76a6f864f5de694345fe4aa2c10d0e4190fc4e4
ejoongseok <ejoongseok@gmail.com>
2022-06-28 14:22:59 +0900

    상품 테스트 케이스 Assertions 추가

--- src/test/java/com/example/appkata/integartion/ProductApiIntegrationTest.java
+++ src/test/java/com/example/appkata/integartion/ProductApiIntegrationTest.java
@@ -1,4 +1,23 @@
 package com.example.appkata.integartion;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
 class ProductApiIntegrationTest {
+
+
+	@Test
+	void 상품_등록() {
+		// given
+
+		// when
+
+		// then
+		Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+		Assertions.assertThat(createProductResponse.getId()).isPositive();
+		Assertions.assertThat(createProductResponse.getProductName()).isEqualTo(productName);
+		Assertions.assertThat(createProductResponse.getPrice()).isEqualTo(price);
+	}
+
 }
