b9a81e31bd81bc0cc7c4fc7b14b25329aadb6484
ejoongseok <ejoongseok@gmail.com>
2022-06-28 14:39:30 +0900

    상품 등록 서비스 테스트 케이스 추가

---
+++ src/test/java/com/example/appkata/module/product/ProductServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.appkata.module.product;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProductServiceTest {
+
+	@Test
+	@DisplayName("상품 등록")
+	void create_product_test() {
+		// given
+
+		// when
+
+		// then
+	}
+
+}
